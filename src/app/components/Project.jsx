4293084010eb1deaca1854807795f2b0df0f5ef8
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-05-11 21:55:08 -0400

    created projects lsit

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -1,7 +1,35 @@
 const projectsList = [
-	{ name: 'TaskMaster', description: '', image: '', alt: '' },
-	{ name: 'Tictactoe', description: '', image: '', alt: '' },
-	{ name: 'Contact', description: '', image: '', alt: '' },
-	{ name: 'Feed app', description: '', image: '', alt: '' },
-	{ name: 'Documentation', description: '', image: '', alt: '' },
+	{
+		name: 'TaskMaster',
+		description:
+			'TaskMaster is a user-friendly task management app to help users stay organized, prioritize tasks, and achieve their goals.It is built with React, tailwind css and ESlint.',
+		image: '',
+		alt: 'taskMaster app avatar',
+	},
+
+	{
+		name: 'Tictactoe',
+		description:
+			'This is a simple tic-tac-toe game from the react documentation tutorial. I followed the tutorial to better understand the fundamentals of react.I add more features to it like timer for each player , restart for restart the game, redo and undo for undo and redo move.It is build with react, css and ESlint.',
+		image: '',
+		alt: 'tictactoe app avatar',
+	},
+
+	{
+		name: 'Contact',
+		description:
+			'This is a contact management application that allows users to easily store and organize their contacts. Users can register their contacts and add important information such as phone numbers, email addresses.',
+		image: '',
+		alt: 'contact app avatar',
+	},
+
+	{ name: 'Feed app', description: '', image: '', alt: 'feed app avatar' },
+
+	{
+		name: 'Documentation',
+		description:
+			'This project is a simple documentation page. it is built with JS,HTML and CSS.It gives a good experience to the user with a sidebar and a light-dark mode.',
+		image: '',
+		alt: 'documentation page avatar',
+	},
 ];
