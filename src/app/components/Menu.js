69ff54af14b0daeae70e22bf4f88d3d7ff87ce1a
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-05-23 22:42:00 -0400

    added close menu behavior on window click

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,23 +1,47 @@
 'use client';
 import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 export default function Menu() {
 	const [showMenu, setShowMenu] = useState(false);
+	const menuRef = useRef(null);
 
 	const toggleMenu = () => {
 		setShowMenu(!showMenu);
 	};
 
 	const menuClass = showMenu ? ' menu-container show-menu ' : 'menu-container';
+	useEffect(() => {
+		const closeMenu = (e) => {
+			if (e.target.classList.contains('btn-menu')) {
+				if (menuRef.current.classList.contains('show-menu')) {
+					setShowMenu(false);
+				} else {
+					setShowMenu(true);
+				}
+			} else {
+				setShowMenu(false);
+			}
+		};
+
+		// Add event listener to window object
+		window.addEventListener('click', closeMenu);
+
+		// Clean up the event listener on component unmount
+		return () => {
+			window.removeEventListener('click', closeMenu);
+		};
+	}, []);
 
 	return (
 		<nav className='navbar'>
-			<button type='button' className='btn-menu' onClick={toggleMenu}>
+			<button type='button' className='btn-menu'>
 				menu <Image src='/arrow.svg' width={15} height={10} />
 			</button>
-			<div className={menuClass}>
+			<div className={menuClass} ref={menuRef}>
 				<ul className='menu-list'>
 					<li className='menu-item'>
 						<Link href='/'>Home</Link>
