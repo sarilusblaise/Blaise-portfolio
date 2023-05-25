1715a9b79059db91843daac51461f97e059683bc
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-05-25 08:25:11 -0400

    refactored head

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import './globals.css';
+import Head from 'next/head';
 import { Suspense } from 'react';
 import LoadingPage from './loading';
 import Menu from './components/Menu';
 export const metadata = {
-	title: 'Sarilus Blaise Portfolio',
+	title: 'SARILUS BLONDY WADLEY - Portfolio | Fullstack Web Developer',
 	description: 'Sarilus Blondy Wadley | Full-Stack Developer',
 	keywords:
-		'web development, web design, javascript, react, node, angular, vue, html, css',
+		' Sarilus ,Blondy, Wadley, Balise ,web development, web design, javascript, react, node, html, css, frontend developer,backend developer,programming, react developer',
 };
 
 export default function RootLayout({ children }) {
